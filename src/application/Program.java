6cdb441da22e0896b65762a80251af49e1a45d2d
MartinsOliveira <ivaniomartins1991@gmail.com>
2020-09-11 18:44:57 -0300

    Implementacao do findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,18 @@
 package application;
 
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj = new Department(1, "Book");
-		Seller seller = new Seller(21, "Ivanio", "ivanio@gmail.com", new Date(), 3000.00, obj);
-
-		
-		System.out.println(seller);
+	
+	SellerDao sellerdao  = DaoFactory.createSellerDao();
+	Seller seller = sellerdao.findById(3);
+	System.out.println(seller);
 
 	}
 
