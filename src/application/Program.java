6d7b1bb9dc8bfe6bfeadb9287876349281c1fadc
MartinsOliveira <ivaniomartins1991@gmail.com>
2020-09-11 17:11:45 -0300

    Classe de entidades Vendendor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,22 @@
 package application;
 
+
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Department obj = new Department(1, "Book");
+		Seller seller = new Seller(21, "Ivanio", "ivanio@gmail.com", new Date(), 3000.00, obj);
+
 		
-		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
 }
+	
